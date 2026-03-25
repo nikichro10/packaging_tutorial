806aa18650f0fe52356e8d1eda080d4780089671
Chromik <niklas.chromik@dlr.de>
2026-03-25 13:58:40 +0100

    distance test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from first_package.example import MyAnimate
 
 @pytest.fixture
 def example():
@@ -7,3 +8,14 @@ def example():
 
 def test_dummy(example):
     assert example == 3
+
+def test_distance():
+    p1 = [0, 0]
+    p2 = [3, 4]
+    assert MyAnimate.distance(p1, p2) == 5
+    assert MyAnimate.distance(p2, p1) == MyAnimate.distance(p1, p2)
+    assert MyAnimate.distance(p2, p1).shape == (1,)
+    assert MyAnimate.distance(p2, p2) == 0
+
+
+
